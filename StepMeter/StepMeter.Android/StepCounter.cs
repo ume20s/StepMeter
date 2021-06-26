e115f0dcdb2a686a214ec7d483f65442be62d378
ume20s <ume20s@gmail.com>
2021-06-26 15:32:52 +0900

    プロジェクト ファイルを追加します。

---
+++ StepMeter/StepMeter.Android/StepCounter.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Android.Runtime;
+using Android.Hardware;
+
+[assembly: Dependency(typeof(StepMeter.Droid.StepCounter))]
+
+namespace StepMeter.Droid
+{
+    class StepCounter : Java.Lang.Object, IStepCounter, ISensorEventListener
+    {
+        private int StepsCounter = 1;
+        private SensorManager sManager;
+
+        public int Steps
+        {
+            get { return StepsCounter; }
+            set { StepsCounter = value; }
+        }
+
+        public new void Dispose()
+        {
+            sManager.UnregisterListener(this);
+            sManager.Dispose();
+        }
+
+        public void InitSensorService()
+        {
+            sManager = Android.App.Application.Context.GetSystemService(Context.SensorService) as SensorManager;
+            sManager.RegisterListener(this, sManager.GetDefaultSensor(SensorType.StepDetector), SensorDelay.Normal);
+        }
+
+        public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        {
+            // Console.WriteLine("OnAccuracyChanged called");
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            switch (e.Sensor.Type)
+            {
+                case SensorType.StepCounter:
+                    // StepsCounter++;
+                    break;
+                case SensorType.StepDetector:
+                    StepsCounter++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void StopSensorService()
+        {
+            sManager.UnregisterListener(this);
+        }
+    }
+}
---
+++ StepMeter/StepMeter/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StepMeter
+{
+    public interface IStepCounter
+    {
+        int Steps { get; set; }
+        void InitSensorService();
+        void StopSensorService();
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        IStepCounter StepCounter = DependencyService.Get<IStepCounter>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            StepCounter.InitSensorService();
+        }
+
+        void CountClicked(object o, EventArgs e)
+        {
+            LabelCounter.Text = StepCounter.Steps.ToString();
+        }
+
+        void ResetClicked(object o, EventArgs e)
+        {
+            StepCounter.Steps = 0;
+            LabelCounter.Text = StepCounter.Steps.ToString();
+        }
+
+    }
+}
